faeab96afc0915c6ab328b0a80e947021db53e25
Mayur Upadhayay <mayur123a@gmail.com>
2024-03-31 22:57:41 +0530

    [chore]: Decomposing the Products code into semantic components for SEP

---
+++ solid/src/components/principles/SRP/Products.tsx
@@ -0,0 +1,25 @@
+// Top level definition
+import { ReactElement } from "react";
+
+// Constituent components
+import { Product } from "./Product";
+
+// types
+import type { IProduct } from "../../../utils/types";
+
+// props type definition
+interface IProps {
+    products: Array<IProduct>
+}
+
+// Comonent definition
+export function Products({ products = [] }: IProps): ReactElement {
+    return (
+        <div className="h-full flex flex-wrap justify-center">
+            {products.map((product: IProduct) => (
+                <Product product={product} />
+            ))}
+        </div>
+    )
+};
+
